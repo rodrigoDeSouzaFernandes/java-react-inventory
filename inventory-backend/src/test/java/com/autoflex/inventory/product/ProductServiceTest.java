3f20053421103e96a6bd7f691d3da01e337b8e37
Rodrigo <rdesouza2008@gmaill.com>
2026-02-16 09:26:54 -0300

    tests: imporove products service test

--- inventory-backend/src/test/java/com/autoflex/inventory/product/ProductServiceTest.java
+++ inventory-backend/src/test/java/com/autoflex/inventory/product/ProductServiceTest.java
@@ -63,7 +63,7 @@ class ProductServiceTest {
     void shouldReturnAllProductsWhenProductibleOnlyFalse() {
         Product product = new Product();
         product.setId(1L);
-        product.setName("Produto");
+        product.setName("Product");
         product.setValue(BigDecimal.valueOf(100));
         product.setRawMaterials(List.of());
 
@@ -72,21 +72,41 @@ class ProductServiceTest {
         List<ProductWithQuantityDTO> result = productService.listProductsWithQuantity(false);
 
         assertEquals(1, result.size());
-        assertEquals("Produto", result.get(0).name);
+        assertEquals("Product", result.get(0).name);
     }
 
     @Test
-    void shouldFilterNonProducibleProducts() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Produto");
-        product.setValue(BigDecimal.valueOf(100));
-        product.setRawMaterials(List.of()); // producible = 0
+    void shouldFilterProducibleProducts() {
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setName("Product 1");
+        product1.setValue(BigDecimal.valueOf(100));
+        product1.setRawMaterials(List.of());
 
-        when(productRepository.listAll()).thenReturn(List.of(product));
+        RawMaterial rm1 = new RawMaterial();
+        rm1.setStockQuantity(100);
+
+        RawMaterial rm2 = new RawMaterial();
+        rm2.setStockQuantity(50);
+
+        ProductRawMaterial prm1 = new ProductRawMaterial();
+        prm1.setRawMaterial(rm1);
+        prm1.setRequiredQuantity(10);
+
+        ProductRawMaterial prm2 = new ProductRawMaterial();
+        prm2.setRawMaterial(rm2);
+        prm2.setRequiredQuantity(5);
+
+        Product product2 = new Product();
+        product2.setName("Product 2");
+        product2.setRawMaterials(List.of(prm1, prm2));
+
+
+        when(productRepository.listAll()).thenReturn(List.of(product1, product2));
 
         List<ProductWithQuantityDTO> result = productService.listProductsWithQuantity(true);
 
-        assertEquals(0, result.size());
+        assertEquals(1, result.size());
+        assertEquals(product2.getName(), result.getFirst().name);
     }
 }
